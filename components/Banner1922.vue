773f059fd99de76f342220ca76d06bf8c5297e7c
Arseny Petrovich <strangesound@gmail.com>
2022-05-10 09:24:22 +0300

    first desktop header, sidebar, baner1922

---
+++ components/Banner1922.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="banner1922">
+        <div class="banner1922__left">
+            <div class="banner1922__top-text-container">
+                <p class="banner1922__text">Сыграй в&nbspлегендарном матче</p>
+                <p class="banner1922__year">1922</p>
+            </div>
+            <div class="banner1922__arrow"></div>
+
+            <p class="banner1922__main-text">Каждый желающий может провести свою игру на лодке и стать частью истории
+            </p>
+            <button class="btn">Записаться на матч</button>
+
+        </div>
+        <div class="banner1922__right"></div>
+    </div>
+
+</template>
+
+<style>
+.banner1922 {
+    background-color: var(--colorRed);
+    display: grid;
+    grid-template-columns: 4fr 5fr;
+    min-height: 280px;
+}
+
+.banner1922__left {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding: 8px;
+}
+
+.banner1922__top-text-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between
+}
+
+.banner1922__year {
+    font-family: 'Druk';
+    font-style: italic;
+    font-weight: 400;
+    font-size: 80px;
+    line-height: 85%;
+    letter-spacing: 0.01em;
+    text-transform: uppercase;
+    color: var(--colorLight);
+
+}
+
+.banner1922__text {
+    font-family: 'Helvetica Neue';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 25px;
+    line-height: 96%;
+    letter-spacing: -0.01em;
+    text-transform: uppercase;
+    color: var(--colorLight);
+    width: min-content;
+
+}
+
+.banner1922__main-text{
+        font-family: 'Helvetica Neue';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 25px;
+    line-height: 96%;
+    letter-spacing: -0.01em;
+    /* text-transform: uppercase; */
+    color: var(--colorLight);
+    opacity: .5;
+
+}
+
+.banner1922__arrow {
+    background-image: url(~/assets/images/arr.svg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 16px;
+
+}
+
+.banner1922__right {
+    background-image: url(~/assets/images/1922pattern.svg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+
+
+}
+</style>
---
+++ components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="btn">{{name}}</div>
+</template>
+
+
+<style>
+
+</style>
+
+<script>
+
+
+
+</script>
